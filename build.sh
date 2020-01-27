3d0f097e8e6a320c2316323273fbf43d1d1685ad
Herman van Dompseler <herman@dompseler.nl>
2020-01-27 14:54:17 +0100

    split script in init & build script for jenkins

--- build.sh
+++ build.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-#clean
-rm -rf LtiLauncher
-
-#get repo
-git clone https://github.com/Drieam/LtiLauncher
-
-# insert labels
-cd LtiLauncher
-mv Dockerfile Dockerfile.bak
-head -n 1 Dockerfile.bak >> Dockerfile
-cat ../docker-labels.txt >> Dockerfile
-tail -n +2 Dockerfile.bak >> Dockerfile
-
 #build
+cd LtiLauncher
 docker build -t ltilauncher .
--- readme.md
+++ readme.md
@@ -4,8 +4,12 @@ LTI Launcher
 Dit is de lti launcher, build by drieam,  for the DLO project
 
 
+Init.sh
+-------
+Dit script cloned de repository
+
+
 Build.sh
 --------
-
-Dit script cloned de repository & bouwt het ltilauncher image
+Dit script bouwt het ltilauncher image
 
