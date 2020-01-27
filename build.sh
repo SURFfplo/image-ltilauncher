aab100d23a07d333aaa19d5203c3b3a6f73d24d2
Herman van Dompseler <herman@dompseler.nl>
2020-01-27 14:37:29 +0100

    first version

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#clean
+rm -rf LtiLauncher
+
+#get repo
+git clone https://github.com/Drieam/LtiLauncher
+
+# insert labels
+cd LtiLauncher
+mv Dockerfile Dockerfile.bak
+head -n 1 Dockerfile.bak >> Dockerfile
+cat ../docker-labels.txt >> Dockerfile
+tail -n +2 Dockerfile.bak >> Dockerfile
+
+#build
+docker build -t ltilauncher .
---
+++ readme.md
@@ -0,0 +1,11 @@
+LTI Launcher
+==========
+
+Dit is de lti launcher, build by drieam,  for the DLO project
+
+
+Build.sh
+--------
+
+Dit script cloned de repository & bouwt het ltilauncher image
+
